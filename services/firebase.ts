677a104cfae597a45356fe8251472ff990d02d89
Taibaloprojects <your_email_for_taibaloprojects@domain.com>
2026-06-08 14:57:26 -0400

    fixed new project

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,14 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  // This tells the code to look for a hidden file on your computer for the key
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "gen-lang-client-0527665513.firebaseapp.com",
-  projectId: "gen-lang-client-0527665513",
-  storageBucket: "gen-lang-client-0527665513.firebasestorage.app",
-  messagingSenderId: "221885926112",
-  appId: "1:221885926112:web:868f103682fb0f01641e85",
-  measurementId: "G-KEKB8TQX32"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
